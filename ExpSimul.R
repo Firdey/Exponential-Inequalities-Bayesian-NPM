f4ebf8be1032812c6c28e5d6c8c1eb1722400a1e
firdey <ho.law@spc.ox.ac.uk>
2015-11-02 09:33:46 +0000

    Added Comments

--- ExpSimul.R
+++ ExpSimul.R
@@ -1,23 +1,44 @@
 library(fda)
 library(mvtnorm)
+
 sin_basis<-function(x,n){
   return(sqrt(2)*sin(2*pi*n*x));  
 }
+#Sin Basis Function
+
 cos_basis<-function(x,n){
   return(sqrt(2)*cos(2*pi*n*x));   
 }
+#Cos Basis Function
+
 f_0truth<-function(x){
 f_0=2+1.2*sin_basis(x,1)+1*cos_basis(x,1)+0.8*sin_basis(x,2)+0.7*cos_basis(x,2);
+return(f_0)
 }
+#Truth function we aim to be near to.
+
 SimulationBM<-function(k,alpha,tao,n,sigma){
+#Simulation of the quantity of interest with inputs:
+# k the number of basis (would like to be greater than number of basis of the f_0)
+# alpha, tao and sigma control
+# n the number of data
+  
 x=runif(n,0,1);
+# Generate the covariates (n of these)
+
+#Ignore
 #thetaPri=rep(0,k);
 #for (j in 1:k) {
 #theta[j]=rnorm(1,0,sqrt((tao^2)*j^(-2*alpha-1)));
 #}
+
 epsilon=rnorm(n,0,sigma^2);
-f_0truth_at_x <- f_0truth(x);
+#Generate the errors with sigma 
+
+f_0truth_at_x < f_0truth(x);
 y_data<-f_0truth_at_x + epsilon;
+#Create our observed noise data.
+
 PriorMult=function(theta,tao,alpha,k){
   PriorMult=1;
   for (l in 1:k){
@@ -25,22 +46,31 @@ PriorMult=function(theta,tao,alpha,k){
   }
   return(PriorMult)
 }
+#The prior density multiplied as a function of theta implicitly.
+
 fourier_eval=function(theta,x){
+  #Vector of theta k size and x n size vector.
   k=length(theta);
-  eval = 0;
+  eval = rep(0,length(x));
+  
   for (j in 1:k){
     if (j==1) {
     eval = eval + theta[j];
+    #First coeff of basis is constant
   }
   else if (j%%2==0) {
     eval=eval +theta[j]*sin_basis(x,j/2);
+    #If even then a sin basis, sin basis is always first
+    
 }
 else {
   eval=eval +theta[j]*cos_basis(x,(j-1)/2);
+  #If odd then a cos basis, the index is shifted accordingly.
 }
 }
 return(eval)
 }
+#Evaluates the value of f given theta and x. Note the vectorised format, output a vector of size n.
 Likelihood=function(y_data,x,epsilon,sigma,n,f_0truth_at_x,theta){
   likelihood=1;
   for (s in 1:n){
@@ -48,10 +78,17 @@ Likelihood=function(y_data,x,epsilon,sigma,n,f_0truth_at_x,theta){
   }
   return(likelihood)
 }
+#Evaluates the likelihood, implicitly a function of theta.
+
 PosteriorKernel=function(theta,y_data,x,epsilon,sigma,n,f_0truth_at_x,tao,alpha,k){
   S=Likelihood(y_data,x,epsilon,sigma,n,f_0truth_at_x,theta)*PriorMult(theta,tao,alpha,k);
+  #Obtain posterior Kernel without NC possibly.
+  
 }
-return(MetropolisHastingC(1000,function(theta) {PosteriorKernel(theta,y_data,x,epsilon,sigma,n,f_0truth_at_x,tao,alpha,k)},0.01,rep(0,k),5))
+return(MetropolisHastingC(1000,function(theta) {PosteriorKernel(theta,y_data,x,epsilon,sigma,n,f_0truth_at_x,tao,alpha,k)},0.01,c(2,1,1,1,1),5))
+#Generates 1000 samples from posterior distribution, with independent multivariate gaussian as proposal with 0.01 as variance across diagnoal.
+#Intialisation is at 0 k dimensioanl with thinning at 5.
+
 }
 MetropolisHastingC = function(TamplesN, densityInt, sigma, initial,thin) {
   SamplesN=thin*TamplesN;
